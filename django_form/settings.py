855423372c31083bb19b931474149471951506ca
shinhosuck <shinhosuck1973@gmail.com>
2023-04-02 19:34:39 +0800

    Update settings.py

--- django_form/settings.py
+++ django_form/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n-qxrqo!jkarg)m2q757*7!r9$x#zvyj!n@6e=2nqncec8bmt5'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
